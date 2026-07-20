3749256fd5e1be43ec8add15e2d6f46ba5196798
jade <jade0911k@gmail.com>
2026-07-20 20:45:44 +0900

    오더서비스 > 스케줄러로 데이터 발행/수신 확인

--- orderService/src/main/java/com/example/orderservice/kafka/producer/OutboxProducer.java
+++ orderService/src/main/java/com/example/orderservice/kafka/producer/OutboxProducer.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutionException;
+
 @Component
 @RequiredArgsConstructor
 public class OutboxProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final static String TOPIC = "order-topic";
+    private final static String TOPIC = "order-requested";
 
-    public void sendKafkaEvent(String message) {
-        kafkaTemplate.send(TOPIC, message);
+    public void produce(String key, String message) throws ExecutionException, InterruptedException {
+        kafkaTemplate.send(TOPIC, key, message).get();
     }
 }
